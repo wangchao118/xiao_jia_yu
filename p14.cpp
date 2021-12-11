bb785b703eb4df54c5284b7d1d0addf53dd253ba
wangchao118 <chao8736@126.com>
2021-12-11 20:46:26 +0800

    second

--- p14.cpp
+++ p14.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define FULL_GAS 85
+
 class Car
 {
 public:
@@ -12,7 +14,8 @@ public:
     void setEngine(std::string eng);
     void setwheel(unsigned int whe);
     void filltank(float liter);
-    void running();
+    int running();
+    void warning();
 };
 
 void Car::setColor(std::string col)
@@ -30,13 +33,43 @@ void Car::filltank(float liter)     //作用域解析符
     gas_tank += liter;
 }
 
-void Car::running()
+int Car::running()
 {
+    std::cout<<"我正在以120的时速在往前移动\n";
+    gas_tank--;
+    std::cout<<"当前还剩"<<100*gas_tank/FULL_GAS<<"%"<<"油量！";
 
+    return gas_tank;
+}
+
+void Car::warning()
+{
+    std::cout<<"Warning!!"<<"还剩"<<100*gas_tank/FULL_GAS<<"%"<<"油量！";
 }
 
 int main()
 {
-    std::cout<<"123333"<<std::endl;
+    char i;
+    Car mycar;
+    mycar.setColor("WHITE");
+    mycar.setEngine("V8");
+    mycar.setwheel(4);
+
+    mycar.gas_tank = FULL_GAS;
+
+    while(mycar.running())
+    {
+        if(mycar.running()<10)
+        {
+            mycar.warning();
+            std::cout<<"请问是否需要加满油再行驶（Y/N）\n";
+            std::cin>>i;
+            if (i =='Y' || i == 'y')
+            {
+                mycar.filltank(FULL_GAS);
+            }
+        }
+    }
+
     return 0;
 }
